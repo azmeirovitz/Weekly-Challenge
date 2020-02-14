63bb99697937176be47518efda8c412a676e297a
Ariel <arcomp123@gmail.com>
2020-02-14 00:00:25 -0800

    Foundations for backend server.js

--- server.js
+++ server.js
@@ -10,9 +10,41 @@ app.use(express.json());
 
 
 
-
-
 ///THE CODE WITH DB.QUERY OR EXECUTE:
+
+app.options("*", async (req, res) => {
+    res.setHeader("access-control-allow-origin", "*");
+    res.setHeader("access-control-allow-headers", "*");
+    res.send();
+}) // K's
+
+
+app.post('/api/student_info', async (req, res, next) => {
+
+console.log("req.body: ", req.body);
+
+
+res.setHeader("access-control-allow-origin", "*"); // K's
+
+    const {firstName, lastName, email, password } = req.body;
+
+    const [results] = await db.execute(`INSERT INTO student_info (pid, firstName, lastName, email, password_hashed) VALUES (UUID(), ?, ?, ?, ?)`, [firstName, lastName, email, password]);
+
+    const [studentData] = await db.query(`SELECT pid, firstName, lastName, email,  password_hashed AS password FROM student_info WHERE firstName = ? AND lastName = ? AND email = ? AND password_hashed`, [firstName, lastName, email, password]);
+
+res.send({
+    message: 'Inserting a new student works!',
+    
+    studentInfo: studentData,
+
+    
+});
+
+
+});
+
+
+
 
 
 
